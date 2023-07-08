a2cf5672fae00cf179df6f5ef8b4af8168cfdca0
Lara Reyes <126874982+Lara-Reyes@users.noreply.github.com>
2023-07-08 18:09:36 -0300

    Fetch data con axios. Mapeo de la data en CountryCard. Ordenar alfabeticamente y estilos en countryCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import DropDownMenu from "./components/DropDownMenu";
 import CountryCard from "./components/CountryCard";
 
 function App() {
+  const [countryData, setCountryData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://restcountries.com/v3.1/all");
+        const sortedData = response.data.sort((a, b) => {
+          const countryA = a.name.common.toUpperCase();
+          const countryB = b.name.common.toUpperCase();
+          if (countryA < countryB) {
+            return -1;
+          }
+          if (countryA > countryB) {
+            return 1;
+          }
+          return 0;
+        });
+        setCountryData(sortedData);
+      } catch (error) {
+        console.error("Error retrieving country data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="bg-day-lightBackground">
       <Header />
       <SearchBar />
       <DropDownMenu />
-      <CountryCard/>
+      <div className="flex flex-wrap justify-center gap-10">
+        {countryData.map((country, index) => (
+          <CountryCard key={index} country={country} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 
-function CountryCard() {
+function CountryCard({ country }) {
+  const { flags, name, population, region, capital } = country;
+
   return (
     <div className="flex justify-center">
-      <div className="w-80 flex-col rounded overflow-hidden shadow-md">
+      <div className="w-72 md:w-80 flex-col rounded overflow-hidden shadow-md pb-6">
         <img
-          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAElBMVEUAAAD/zgDdAADnAADaAAD/2AAtsSEoAAAA+ElEQVR4nO3QMQGAMAAEsYeCf8tIuI0pkZANAAAAAAAAAAAAAAAAAAAAgB8dwm6CoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKewh7CbsIipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUofMGTNC8HkSxoAAAAASUVORK5CYII="
-          alt=""
-          className="w-full"
+          src={flags.png}
+          alt="Flag"
+          className="w-full h-40 md:h-48 object-cover"
         />
         <div className="px-6 py-4">
-          <h2 className="font-bold text-xl mb-2">Germany</h2>
+          <h2 className="font-bold text-xl mb-2">{name.common}</h2>
           <p>
-            <span>Population:</span>
-            81,770,900
+            <span>Population:</span> {population}
           </p>
           <p>
-            <span>Region:</span>
-            Europe
+            <span>Region:</span> {region}
           </p>
           <p>
-            <span>Capital:</span>
-            Berlin
+            <span>Capital:</span> {capital && capital[0]}
           </p>
         </div>
       </div>
